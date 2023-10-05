c1c0de38a8361c42df32ce077d8b341e22229b35
Li 'oldrev' Wei <oldrev@gmail.com>
2023-10-05 11:22:52 +0800

    update build toolchain

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ pushd "$BASEDIR"
 
 mkdir -p build
 
-conan install . --output-folder=build --build=missing --settings=build_type=Debug -pr profiles/x86_64-linux.ini
-source ./build/conanbuild.sh
-cmake -B build -G Ninja -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Debug
+conan install . --output-folder=build/conan --build=missing --settings=build_type=Debug -pr profiles/x86_64-linux.ini
+source ./build/conan/conanbuild.sh
+cmake -B build -G Ninja -DCMAKE_TOOLCHAIN_FILE=conan/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Debug
 cmake --build build
