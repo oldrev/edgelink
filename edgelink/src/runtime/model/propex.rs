47eb39d0178d2f4650cf22ca212708ccbba5f814
Li Wei <oldrev@gmail.com>
2023-10-25 22:49:19 +0800

    update

--- edgelink/src/runtime/model/propex.rs
+++ edgelink/src/runtime/model/propex.rs
@@ -6,7 +6,7 @@ pub enum PropexError {
     BadArguments,
 
     #[error("Invalid Propex syntax")]
-    BadSyntax,
+    BadSyntax(String),
 
     #[error("Invalid number digit")]
     InvalidDigit,
@@ -31,84 +31,86 @@ pub fn parse(expr: &str) -> Result<Vec<PropexSegment<'_>>, PropexError> {
     let mut segs = Vec::new();
     let mut _levels: usize = 0;
 
-    eval.skip_whitespace()?;
+    eval.skip_whitespace();
 
-    while eval.pos.get() < eval.expr.len() {
+    while eval.is_available() {
         /*
         if levels >= PROPEX_MAX_LEVELS {
             return Err(PropexError::BadArguments);
         }
         */
-        eval.skip_whitespace()?;
-        if eval.peek()? == '.' {
-            eval.forward(1)?;
+        eval.skip_whitespace();
+        if eval.peek() == '.' {
+            eval.forward(1);
             continue;
-        } else if eval.peek()? == '[' {
-            eval.forward(1)?;
-            eval.skip_whitespace()?;
-            if eval.peek()? == '\'' || eval.peek()? == '"' {
+        } else if eval.peek() == '[' {
+            eval.forward(1);
+            eval.skip_whitespace();
+            if eval.peek() == '\'' || eval.peek() == '"' {
                 let seg = eval.parse_string_index()?;
                 segs.push(seg);
             } else {
                 let seg = eval.parse_integer_index()?;
                 segs.push(seg);
             }
-            eval.skip_whitespace()?;
-            if eval.peek()? != ']' {
-                return Err(PropexError::BadSyntax);
+            eval.skip_whitespace();
+            if eval.peek() != ']' {
+                return Err(PropexError::BadSyntax("Unmatched ']'".to_string()));
             }
-            eval.forward(1)?;
-        } else if eval.peek()?.is_ascii_alphabetic() || eval.peek()? == '_' {
+            eval.forward(1);
+        } else if eval.peek().is_ascii_alphabetic() || eval.peek() == '_' {
             let seg = eval.parse_property()?;
             segs.push(seg);
+            assert_eq!(eval.peek(), '.');
         } else {
-            return Err(PropexError::BadSyntax);
+            return Err(PropexError::BadSyntax(
+                "Bad property expression".to_string(),
+            ));
         }
 
         _levels += 1;
-        eval.skip_whitespace()?;
+        eval.skip_whitespace();
     }
 
     Ok(segs)
 }
 
 impl<'a> PropexEval<'a> {
-    fn forward(&self, n: usize) -> Result<char, PropexError> {
-        self.pos.set(self.pos.get() + n);
+    fn is_available(&self) -> bool {
+        self.pos.get() < self.expr.len()
+    }
+
+    fn forward(&self, n: usize) -> char {
+        let new_pos = self.pos.get() + n;
+        assert!(new_pos < self.expr.len());
+        self.pos.set(new_pos);
         self.peek()
     }
 
-    fn peek(&self) -> Result<char, PropexError> {
-        if self.pos.get() >= self.expr.len() {
-            Err(PropexError::BadSyntax)
-        } else if let Some(c) = self.expr.chars().nth(self.pos.get()) {
-            Ok(c)
-        } else {
-            Err(PropexError::BadSyntax)
+    fn peek(&self) -> char {
+        assert!(self.pos.get() < self.expr.len());
+        self.expr.chars().nth(self.pos.get()).unwrap()
+    }
+
+    fn forward_when(&self, pred: fn(char) -> bool) {
+        while pred(self.peek()) {
+            self.forward(1);
         }
     }
 
-    fn forward_when(&self, pred: fn(char) -> bool) -> Result<(), PropexError> {
-        while pred(self.peek()?) {
-            self.forward(1)?;
+    fn scan_for(&self, pred: fn(char) -> bool) {
+        while self.is_available() && pred(self.peek()) {
+            self.forward(1);
         }
-        Ok(())
     }
 
-    fn scan_for(&self, pred: fn(char) -> bool) -> Result<(), PropexError> {
-        while pred(self.peek()?) {
-            self.forward(1)?;
-        }
-        Ok(())
-    }
-
-    fn skip_whitespace(&self) -> Result<(), PropexError> {
+    fn skip_whitespace(&self) {
         self.forward_when(|c| c == '\t' || c == ' ')
     }
 
     fn parse_integer_index(&self) -> Result<PropexSegment<'a>, PropexError> {
         let start_pos = self.pos.get();
-        self.forward_when(|c| c.is_ascii_digit())?;
+        self.forward_when(|c| c.is_ascii_digit());
         let digits = &self.expr[start_pos..self.pos.get()];
         match digits.parse::<usize>() {
             Ok(index) => Ok(PropexSegment::IntegerIndex(index)),
@@ -117,11 +119,8 @@ impl<'a> PropexEval<'a> {
     }
 
     fn parse_property(&self) -> Result<PropexSegment<'a>, PropexError> {
-        self.forward(1)?; // skip '\'' or '"'
         let start_pos = self.pos.get();
-        if self.peek()?.is_ascii_alphabetic() || self.peek()? == '_' {
-            self.scan_for(|c| c.is_ascii_alphanumeric() || c == '_')?;
-        }
+        self.scan_for(|c| c.is_ascii_alphanumeric() || c == '_');
 
         Ok(PropexSegment::StringIndex(
             &self.expr[start_pos..self.pos.get()],
@@ -129,20 +128,39 @@ impl<'a> PropexEval<'a> {
     }
 
     fn parse_string_index(&self) -> Result<PropexSegment<'a>, PropexError> {
-        self.forward(1)?; // skip '\'' or '"'
+        self.forward(1); // skip '\'' or '"'
         let start_pos = self.pos.get();
 
         // scan for string content
-        self.scan_for(|c| c == '\'' || c == '"')?;
+        self.scan_for(|c| c == '\'' || c == '"');
 
-        if self.peek()? != '\'' && self.peek()? != '"' {
-            return Err(PropexError::BadSyntax);
+        if self.peek() != '\'' && self.peek() != '"' {
+            return Err(PropexError::BadSyntax(
+                "Unmatched string quotation".to_string(),
+            ));
         }
-        _ = self.forward(1)?;
+        _ = self.forward(1);
         Ok(PropexSegment::StringIndex(
             &self.expr[start_pos..self.pos.get()],
         ))
     }
+}
+
+#[test]
+fn all_primitives_should_work() {
+    let white_expr = "   aaabcd";
+    let eval = PropexEval {
+        expr: white_expr,
+        pos: std::cell::Cell::new(0),
+    };
+    eval.skip_whitespace();
+    assert_eq!(3, eval.pos.get());
+    eval.forward(1);
+    assert_eq!('a', eval.peek());
+    eval.scan_for(|c| c == 'a');
+    assert_eq!('b', eval.peek());
+    eval.forward(2);
+    assert_eq!('d', eval.peek());
 }
 
 #[test]
