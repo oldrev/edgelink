caac2a1c4772d8b9a3e517aa3dd08b8084ac95da
Li 'oldrev' Wei <oldrev@gmail.com>
2023-10-26 17:23:10 +0800

    尝试更新

--- edgelink/src/runtime/model/propex.rs
+++ edgelink/src/runtime/model/propex.rs
@@ -1,5 +1,17 @@
 use thiserror::Error;
 
+use nom::{
+    branch::alt,
+    bytes::complete::{escaped, tag, take_while},
+    character::complete::{alphanumeric1 as alphanumeric, char, one_of},
+    combinator::{cut, map, opt, value},
+    error::{context, convert_error, ContextError, ErrorKind, ParseError, VerboseError},
+    multi::separated_list0,
+    number::complete::double,
+    sequence::{delimited, preceded, separated_pair, terminated},
+    Err, IResult, Parser,
+};
+
 #[derive(Error, Debug)]
 pub enum PropexError {
     #[error("Invalid arguments")]
@@ -18,155 +30,63 @@ pub enum PropexSegment<'a> {
     StringIndex(&'a str), // Use a reference to a string slice
 }
 
-struct PropexEval<'a> {
-    expr: &'a str,
-    pos: std::cell::Cell<usize>,
+#[derive(Debug)]
+enum NavigationElement {
+    Field(String),
+    StringIndex(String),
+}
+
+fn skip_spaces<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, &'a str, E> {
+    let chars = " \t\r\n";
+    take_while(move |c| chars.contains(c))(i)
+}
+
+fn parse_str<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, &'a str, E> {
+    escaped(alphanumeric, '\\', one_of("\"n\\"))(i)
+}
+
+fn parse_double_quoted_string<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
+    i: &'a str,
+) -> IResult<&'a str, &'a str, E> {
+    context(
+        "double_quoted_string",
+        preceded(char('\"'), cut(terminated(parse_str, char('\"')))),
+    )(i)
+}
+
+fn parse_single_quoted_string<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
+    i: &'a str,
+) -> IResult<&'a str, &'a str, E> {
+    context(
+        "double_quoted_string",
+        preceded(char('\''), cut(terminated(parse_str, char('\'')))),
+    )(i)
 }
 
 pub fn parse(expr: &str) -> Result<Vec<PropexSegment<'_>>, PropexError> {
-    let eval = PropexEval {
-        expr,
-        pos: std::cell::Cell::new(0),
-    };
     let mut segs = Vec::new();
     let mut _levels: usize = 0;
 
-    eval.skip_whitespace();
+    /*
+    let mut lex = Token::lexer(expr);
 
-    while eval.is_available() {
-        /*
-        if levels >= PROPEX_MAX_LEVELS {
-            return Err(PropexError::BadArguments);
-        }
-        */
-        eval.skip_whitespace();
-        if eval.peek() == '.' {
-            eval.forward(1);
-            continue;
-        } else if eval.peek() == '[' {
-            eval.forward(1);
-            eval.skip_whitespace();
-            if eval.peek() == '\'' || eval.peek() == '"' {
-                let seg = eval.parse_string_index()?;
-                segs.push(seg);
-            } else {
-                let seg = eval.parse_integer_index()?;
-                segs.push(seg);
-            }
-            eval.skip_whitespace();
-            if eval.peek() != ']' {
-                return Err(PropexError::BadSyntax("Unmatched ']'".to_string()));
-            }
-            eval.forward(1);
-        } else if eval.peek().is_ascii_alphabetic() || eval.peek() == '_' {
-            let seg = eval.parse_property()?;
-            segs.push(seg);
-            assert_eq!(eval.peek(), '.');
-        } else {
-            return Err(PropexError::BadSyntax(
-                "Bad property expression".to_string(),
-            ));
-        }
-
-        _levels += 1;
-        eval.skip_whitespace();
+    while let Some(token) = lex.next() {
+        let seg = match token {
+            Ok(Token::Dot) => continue,
+            Ok(Token::DoubleQuotedString) => PropexSegment::StringIndex(lex.slice()),
+            Err(err) => return Err(PropexError::BadSyntax("Syntax error".to_string())),
+            _ => PropexSegment::IntegerIndex(123),
+        };
+        segs.push(seg);
     }
+    */
 
     Ok(segs)
 }
 
-impl<'a> PropexEval<'a> {
-    fn is_available(&self) -> bool {
-        self.pos.get() < self.expr.len()
-    }
-
-    fn forward(&self, n: usize) -> char {
-        let new_pos = self.pos.get() + n;
-        assert!(new_pos < self.expr.len());
-        self.pos.set(new_pos);
-        self.peek()
-    }
-
-    fn peek(&self) -> char {
-        assert!(self.pos.get() < self.expr.len());
-        self.expr.chars().nth(self.pos.get()).unwrap()
-    }
-
-    fn forward_when(&self, pred: fn(char) -> bool) {
-        while pred(self.peek()) {
-            self.forward(1);
-        }
-    }
-
-    fn scan_for(&self, pred: fn(char) -> bool) {
-        while self.is_available() && pred(self.peek()) {
-            self.forward(1);
-        }
-    }
-
-    fn skip_whitespace(&self) {
-        self.forward_when(|c| c == '\t' || c == ' ')
-    }
-
-    fn parse_integer_index(&self) -> Result<PropexSegment<'a>, PropexError> {
-        let start_pos = self.pos.get();
-        self.forward_when(|c| c.is_ascii_digit());
-        let digits = &self.expr[start_pos..self.pos.get()];
-        match digits.parse::<usize>() {
-            Ok(index) => Ok(PropexSegment::IntegerIndex(index)),
-            _ => Err(PropexError::InvalidDigit),
-        }
-    }
-
-    fn parse_property(&self) -> Result<PropexSegment<'a>, PropexError> {
-        let start_pos = self.pos.get();
-        self.scan_for(|c| c.is_ascii_alphanumeric() || c == '_');
-
-        Ok(PropexSegment::StringIndex(
-            &self.expr[start_pos..self.pos.get()],
-        ))
-    }
-
-    fn parse_string_index(&self) -> Result<PropexSegment<'a>, PropexError> {
-        self.forward(1); // skip '\'' or '"'
-        let start_pos = self.pos.get();
-
-        // scan for string content
-        self.scan_for(|c| c == '\'' || c == '"');
-
-        if self.peek() != '\'' && self.peek() != '"' {
-            return Err(PropexError::BadSyntax(
-                "Unmatched string quotation".to_string(),
-            ));
-        }
-        _ = self.forward(1);
-        Ok(PropexSegment::StringIndex(
-            &self.expr[start_pos..self.pos.get()],
-        ))
-    }
-}
-
-#[test]
-fn all_primitives_should_work() {
-    let white_expr = "   aaabcd";
-    let eval = PropexEval {
-        expr: white_expr,
-        pos: std::cell::Cell::new(0),
-    };
-    eval.skip_whitespace();
-    assert_eq!(3, eval.pos.get());
-    eval.forward(1);
-    assert_eq!('a', eval.peek());
-    eval.scan_for(|c| c == 'a');
-    assert_eq!('b', eval.peek());
-    eval.forward(2);
-    assert_eq!('d', eval.peek());
-}
-
 #[test]
 fn parse_propex_should_be_ok() {
-    //let expr1 = "test1.hello .world[ 'aaa' ].name_of";
-    let expr1 = "test1.name_of";
+    let expr1 = "test1.hello .world['aaa'].name_of";
     let segs = parse(expr1).unwrap();
 
     match segs[0] {
