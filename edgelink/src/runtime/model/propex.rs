f0c2e6ade6e9d0471a33637f0c21dda5d144e9c4
Li 'oldrev' Wei <oldrev@gmail.com>
2023-10-29 00:08:19 +0800

    基本实现 propex

--- edgelink/src/runtime/model/propex.rs
+++ edgelink/src/runtime/model/propex.rs
@@ -3,12 +3,12 @@ use thiserror::Error;
 use nom::{
     branch::alt,
     bytes::complete::{escaped, tag, take_while},
-    character::complete::{alphanumeric1 as alphanumeric, char, one_of},
-    combinator::{cut, map, opt, value},
+    character::complete::{alpha1, alphanumeric1, char, digit1, multispace0, one_of},
+    combinator::{cut, iterator, map, map_res, opt, recognize, value},
     error::{context, convert_error, ContextError, ErrorKind, ParseError, VerboseError},
-    multi::separated_list0,
+    multi::{many0, many0_count, separated_list0},
     number::complete::double,
-    sequence::{delimited, preceded, separated_pair, terminated},
+    sequence::{self, delimited, pair, preceded, separated_pair, terminated},
     Err, IResult, Parser,
 };
 
@@ -24,43 +24,112 @@ pub enum PropexError {
     InvalidDigit,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub enum PropexSegment<'a> {
     IntegerIndex(usize),
     StringIndex(&'a str), // Use a reference to a string slice
 }
 
-#[derive(Debug)]
-enum NavigationElement {
-    Field(String),
-    StringIndex(String),
+fn parse_identifier(i: &str) -> IResult<&str, &str, VerboseError<&str>> {
+    recognize(pair(
+        alt((alpha1, tag("_"))),
+        many0_count(alt((alphanumeric1, tag("_")))),
+    ))
+    .parse(i)
 }
 
-fn skip_spaces<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, &'a str, E> {
-    let chars = " \t\r\n";
-    take_while(move |c| chars.contains(c))(i)
+fn parse_double_quoted_string(i: &str) -> IResult<&str, PropexSegment, VerboseError<&str>> {
+    map(
+        context(
+            "double_quoted_string",
+            preceded(char('\"'), cut(terminated(parse_str, char('\"')))),
+        ),
+        PropexSegment::StringIndex,
+    )
+    .parse(i)
+}
+
+fn parse_single_quoted_string(i: &str) -> IResult<&str, PropexSegment, VerboseError<&str>> {
+    map(
+        context(
+            "double_quoted_string",
+            preceded(char('\''), cut(terminated(parse_str, char('\'')))),
+        ),
+        PropexSegment::StringIndex,
+    )
+    .parse(i)
+}
+
+fn parse_quoted_string(i: &str) -> IResult<&str, PropexSegment, VerboseError<&str>> {
+    alt((parse_double_quoted_string, parse_single_quoted_string)).parse(i)
 }
 
 fn parse_str<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, &'a str, E> {
-    escaped(alphanumeric, '\\', one_of("\"n\\"))(i)
+    escaped(alphanumeric1, '\\', one_of("\"n\\"))(i)
 }
 
-fn parse_double_quoted_string<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
-    i: &'a str,
-) -> IResult<&'a str, &'a str, E> {
-    context(
-        "double_quoted_string",
-        preceded(char('\"'), cut(terminated(parse_str, char('\"')))),
-    )(i)
+fn parse_integer_index(i: &str) -> IResult<&str, PropexSegment, VerboseError<&str>> {
+    map_res(digit1, |digit_str: &str| {
+        digit_str.parse::<usize>().map(PropexSegment::IntegerIndex)
+    })
+    .parse(i)
 }
 
-fn parse_single_quoted_string<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
-    i: &'a str,
-) -> IResult<&'a str, &'a str, E> {
-    context(
-        "double_quoted_string",
-        preceded(char('\''), cut(terminated(parse_str, char('\'')))),
-    )(i)
+fn parse_index(i: &str) -> IResult<&str, PropexSegment, VerboseError<&str>> {
+    delimited(
+        delimited(multispace0, char('['), multispace0),
+        alt((
+            parse_double_quoted_string,
+            parse_single_quoted_string,
+            parse_integer_index,
+        )),
+        delimited(multispace0, char(']'), multispace0),
+    )
+    .parse(i)
+}
+
+fn parse_property(i: &str) -> IResult<&str, PropexSegment, VerboseError<&str>> {
+    map(
+        context(
+            "property",
+            delimited(multispace0, parse_identifier, multispace0),
+        ),
+        PropexSegment::StringIndex,
+    )
+    .parse(i)
+}
+
+fn parse_subproperty(i: &str) -> IResult<&str, PropexSegment, VerboseError<&str>> {
+    map(
+        context(
+            "subproperty",
+            preceded(
+                delimited(multispace0, char('.'), multispace0),
+                delimited(multispace0, parse_identifier, multispace0),
+            ),
+        ),
+        PropexSegment::StringIndex,
+    )
+    .parse(i)
+}
+
+fn parse_first_fragment(i: &str) -> IResult<&str, PropexSegment, VerboseError<&str>> {
+    alt((parse_property, parse_index)).parse(i)
+}
+
+fn parse_sub_fragment(i: &str) -> IResult<&str, PropexSegment, VerboseError<&str>> {
+    alt((parse_subproperty, parse_index)).parse(i)
+}
+
+fn parse_nav(i: &str) -> IResult<&str, Vec<PropexSegment>, VerboseError<&str>> {
+    let mut segs = Vec::new();
+    let p = pair(parse_first_fragment, many0(parse_sub_fragment)).parse(i)?;
+    segs.push(p.1 .0.clone());
+    for x in p.1 .1.iter() {
+        segs.push(*x);
+    }
+    // parse_first_fragment.map(move |x| segs.push(x)).parse(i)?;
+    Ok((i, segs))
 }
 
 pub fn parse(expr: &str) -> Result<Vec<PropexSegment<'_>>, PropexError> {
@@ -85,12 +154,40 @@ pub fn parse(expr: &str) -> Result<Vec<PropexSegment<'_>>, PropexError> {
 }
 
 #[test]
-fn parse_propex_should_be_ok() {
-    let expr1 = "test1.hello .world['aaa'].name_of";
-    let segs = parse(expr1).unwrap();
+fn parse_primitives_should_be_ok() {
+    let expr = "'test1'";
+    let (_, parsed) = parse_single_quoted_string(expr).unwrap();
+    assert_eq!(PropexSegment::StringIndex("test1"), parsed);
 
-    match segs[0] {
-        PropexSegment::StringIndex(str) => assert_eq!(str, "hello"),
-        _ => assert!(false),
-    };
+    let expr = "\"test1\"";
+    let (_, parsed) = parse_double_quoted_string(expr).unwrap();
+    assert_eq!(PropexSegment::StringIndex("test1"), parsed);
+
+    let expr = "_test_1";
+    let (_, parsed) = parse_property(expr).unwrap();
+    assert_eq!(PropexSegment::StringIndex("_test_1"), parsed);
+
+    let expr = "['aaa']";
+    let (_, parsed) = parse_index(expr).unwrap();
+    assert_eq!(PropexSegment::StringIndex("aaa"), parsed);
+
+    let expr = "[ 123 ]";
+    let (_, parsed) = parse_index(expr).unwrap();
+    assert_eq!(PropexSegment::IntegerIndex(123), parsed);
 }
+
+#[test]
+fn parse_propex_should_be_ok() {
+    let expr1 = "test1.hello .world['aaa'][333][\"bb\"].name_of";
+    let segs = parse_nav(expr1).unwrap().1;
+
+    assert_eq!(7, segs.len());
+    assert_eq!(PropexSegment::StringIndex("test1"), segs[0]);
+    assert_eq!(PropexSegment::StringIndex("hello"), segs[1]);
+    assert_eq!(PropexSegment::StringIndex("world"), segs[2]);
+    assert_eq!(PropexSegment::StringIndex("aaa"), segs[3]);
+    assert_eq!(PropexSegment::IntegerIndex(333), segs[4]);
+    assert_eq!(PropexSegment::StringIndex("bb"), segs[5]);
+    assert_eq!(PropexSegment::StringIndex("name_of"), segs[6]);
+}
+
