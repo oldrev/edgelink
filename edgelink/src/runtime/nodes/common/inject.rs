288f5bdc6054f3b087fdc442047ab096ac5fb815
Li Wei <oldrev@gmail.com>
2023-10-29 23:24:38 +0800

    update

--- edgelink/src/runtime/nodes/common/inject.rs
+++ edgelink/src/runtime/nodes/common/inject.rs
@@ -8,19 +8,40 @@ use crate::runtime::nodes::*;
 use crate::EdgeLinkError;
 use crate::Result;
 
+struct NodeConfig {
+    repeat: Option<u64>,
+    cron: Option<String>,
+    once: bool,
+    once_delay: Option<u64>,
+}
+
 struct InjectNode {
     base: Arc<BaseFlowNode>,
 }
 
 impl InjectNode {
+    fn create_msg(&self) -> Arc<Msg> {
+        let now = crate::utils::time::unix_now().unwrap();
+        let payload = Variant::from(now);
+        Msg::with_payload(self.base.id, payload)
+    }
+
+    async fn once_task(&self, stop_token: CancellationToken) {
+        let msg = self.create_msg();
+
+        let flow_ref = Weak::upgrade(&self.base().flow).unwrap();
+        flow_ref
+            .fan_out_single_port(self.base.id, 0, &[msg], stop_token.clone())
+            .await
+            .unwrap();
+    }
+
     async fn cron_task(&self, stop_token: CancellationToken) {
         while !stop_token.is_cancelled() {
             // TODO FIXME
-            let delay_result = crate::utils::async_util::delay(
-                Duration::from_secs(2),
-                stop_token.child_token(),
-            )
-            .await;
+            let delay_result =
+                crate::utils::async_util::delay(Duration::from_secs(2), stop_token.child_token())
+                    .await;
             match delay_result {
                 Ok(()) => {
                     let flow_ref = Weak::upgrade(&self.base().flow).unwrap();
@@ -43,6 +64,34 @@ impl InjectNode {
         }
         println!("The CRON task has been stopped.");
     }
+
+    async fn repeat_task(&self, stop_token: CancellationToken) {
+        while !stop_token.is_cancelled() {
+            // TODO FIXME
+            let delay_result =
+                crate::utils::async_util::delay(Duration::from_secs(2), stop_token.child_token())
+                    .await;
+            match delay_result {
+                Ok(()) => {
+                    let flow_ref = Weak::upgrade(&self.base().flow).unwrap();
+                    let now = crate::utils::time::unix_now().unwrap();
+                    let payload = Variant::from(now);
+                    let msg = Msg::with_payload(self.base.id, payload);
+                    flow_ref
+                        .fan_out_single_port(self.base.id, 0, &[msg], stop_token.clone())
+                        .await
+                        .unwrap();
+                }
+                Err(ref err) => match err.downcast_ref().unwrap() {
+                    EdgeLinkError::TaskCancelled => {
+                        println!("Inject task has been cancelled.");
+                        break;
+                    }
+                    _ => break,
+                },
+            };
+        }
+    }
 }
 
 #[async_trait]
@@ -54,7 +103,6 @@ impl NodeBehavior for InjectNode {
     fn name(&self) -> &str {
         &self.base.name
     }
-
 }
 
 #[async_trait]
