3958f4e030ba09e01b28fc18561e5525e3e660a7
Li 'oldrev' Wei <oldrev@gmail.com>
2024-08-18 11:53:23 +0800

    add udp script

---
+++ scripts/udp_send.py
@@ -0,0 +1,13 @@
+import socket
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 7788
+MESSAGE = "Hello, World!"
+
+print("UDP target IP: %s" % UDP_IP)
+print("UDP target port: %s" % UDP_PORT)
+print("message: %s" % MESSAGE)
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT))
